68680ccd6ce98b9242d37a9a18a53d417e8a9198
Felix Schekerka <felixschekerka@gmail.com>
2022-07-13 15:58:14 +0100

    first commit

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from routes.home.route import HomeRoute
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(HomeRoute, '/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
